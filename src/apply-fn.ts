a2c39974c1850d975dd82bf07933aed431d8f6a1
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-21 14:50:51 +0100

    change development tooling configs
    Switch to pnpm.
    
    Update various tooling configurations.
    
    Update deps.

--- src/apply-fn.ts
+++ src/apply-fn.ts
@@ -2,7 +2,7 @@ import { Fn1 } from '@gmjs/generic-types';
 
 export function applyFn<TInput, TOutput>(
   value: TInput,
-  fn: Fn1<TInput, TOutput>
+  fn: Fn1<TInput, TOutput>,
 ): TOutput {
   return fn(value);
 }
